86a0ccc6c6f22df0ee7873e30bba78694a97a28e
Chris Riley <chris.s.riley@gmail.com>
2011-11-26 12:41:21 +1300

    Added offline capability

--- js/time-calculator.js
+++ js/time-calculator.js
@@ -17,6 +17,9 @@ var numOfTimeEntries = 7;
  */
 $(document).ready(function()
 {
+	// Send browser stats to Google
+	setupGoogleAnalytics();
+	
 	// Check the browser is supported or show an error message
 	if (checkBrowserSupported() === false)
 	{
@@ -731,6 +734,22 @@ function checkBrowserSupported()
 	else {
 		return false;
 	}
+}
+
+/**
+ * Send browser statistics etc to Google Analytics
+ */
+function setupGoogleAnalytics()
+{
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-27217678-1']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
 }
 
 /**
